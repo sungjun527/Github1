45b0a26a6a56a0337a81457666502916a299930b
sungjun527 <sungjun527@gmail.com>
2015-04-10 22:52:48 +0900

    github test using datastructureassign

--- DataStructure/src/assign2/command/Command.java
+++
@@ -1,6 +0,0 @@
-package assign2.command;
-import assign2.MovieDatabase;
-import assign2.exceptions.*;
-public interface Command {
-	void apply(MovieDatabase db, String args) throws DatabaseException;
-}
